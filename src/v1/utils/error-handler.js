b28905236835ee8356211db6c6e01dd08ad9b20b
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2019-10-17 19:34:24 +0100

    refactor error handler

--- src/v1/utils/error-handler.js
+++ src/v1/utils/error-handler.js
@@ -1,6 +1,6 @@
 import config from '../config/index';
 
-export default (error, req, res) => {
+export default (error, req, res, next) => {
   const response = { message: error.message || 'Error processing request' };
   if (config.NODE_ENV !== 'production') {
     response.developer_message = error;
